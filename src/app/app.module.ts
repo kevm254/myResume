183f978d49d23e4ba2e7405be5cb2ffb5e43f8e1
K M <kevin m>
2018-07-16 18:54:59 -0500

    adding components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
+// Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 
+// Components
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
+    HeaderComponent,
+    FooterComponent
   ],
+
+  // Modules
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(AppRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/content/home/home.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'home-cmp',
+  template: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+}
+
+
